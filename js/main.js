711c61e300b0508c299b2101f805933f896424a0
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-03-29 17:39:00 -0400

    working box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 var $body = $('body');
+var $box = $('.box');
+var $showbtn = $('.btn-show-hide');
 
-$bshowbtn.on('click', '.btn-show-hide', function () {
-  $(this).toggleClass('box');
+$showbtn.on('click', function () {
+  $box.toggleClass('box-trans');
 });
+
+//$body.on('click', '.btn-show-hide', function () {
+//  $(this).toggleClass('box');
+//});
+
+//$body.on('transitionend', '.bubble', function () {
+//  $(this).remove();
+//});
