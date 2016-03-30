3ede2e9afedf6b3760ca5170da1396c1b1fbbdde
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-03-29 20:29:38 -0400

    finnishing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 var $body = $('body');
+var $panel = $('.panel');
 var $box = $('.box');
 var $showbtn = $('.btn-show-hide');
+var $movebtn = $('.btn-move');
+var $diamond = $('.diamond');
+var $collapsebtn = $('.btn-collapse-expand');
+var $circle = $('.circle');
+var $bouncebtn = $('.btn-bounce');
+var $list = $('.list');
+var $appendbtn = $('.btn-append');
 
 $showbtn.on('click', function () {
   $box.toggleClass('box-trans');
 });
 
-//$body.on('click', '.btn-show-hide', function () {
-//  $(this).toggleClass('box');
-//});
+$movebtn.on('click', function () {
+  $diamond.toggleClass('diamond-move');
+});
 
-//$body.on('transitionend', '.bubble', function () {
-//  $(this).remove();
-//});
+$collapsebtn.on('click', function () {
+  $panel.toggleClass('collapser');
+});
+
+$bouncebtn.on('click', function () {
+  $circle.addClass('bouncing');
+});
+
+$bouncebtn.on('animationend', function () {
+  $circle.removeClass('bouncing');
+});
+
+$appendbtn.on('click', function () {
+  $list.append('<li>BOO!</li>');
+});
